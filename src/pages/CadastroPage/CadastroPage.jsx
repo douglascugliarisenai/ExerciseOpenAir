329902b34f389fb75d7debcdc79e05bf3e676783
douglascugliaridev <douglas.cugliari@gmail.com>
2024-04-23 11:48:08 -0300

    feat: 🎸 CARD002-implementacao-pagina-cadastro
    Implementação da tela de cadastro de usuário
    
    ✅ Closes: CARD002

---
+++ src/pages/CadastroPage/CadastroPage.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import CadastroForm from "../../components/molecules/CadastroForm";
+
+function CadastroPage() {
+ return (
+  <>
+   <CadastroForm />
+  </>
+ );
+}
+
+export default CadastroPage;
